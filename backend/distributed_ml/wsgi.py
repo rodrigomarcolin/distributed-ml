2f4f119d294b273ee3f11c0e67d18feb7fa5bf7e
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2023-09-28 17:59:13 -0300

    update wsgi

--- backend/distributed_ml/wsgi.py
+++ backend/distributed_ml/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for distributed_ml project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/rodrigosm29/distributed-ml/backend'
+
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'distributed_ml.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "distributed_ml.settings")
-
 application = get_wsgi_application()
